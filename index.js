fb014fd9609ec966530e96c3a260524764b810d4
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-09-25 08:57:48 +0300

    Complete function lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickBlock) {
+    
+    const headquatersBlock = 42;
+
+    const distanceFromHqInBlocks = Math.abs(pickBlock - headquatersBlock);
+
+    return distanceFromHqInBlocks;
+}
+
+function distanceFromHqInFeet(pickupBlock) {
+    
+    const distanceInBlocks = distanceFromHqInBlocks(pickupBlock);
+
+    const distanceInFeet = distanceInBlocks * 264;
+
+    return distanceInFeet;
+}
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const blockLength = 264; // Length of one block in feet
+    // Calculate the absolute difference in blocks between startBlock and endBlock
+    const blocksTravelled = Math.abs(endBlock - startBlock);
+    // Calculate the distance in feet
+    const distanceInFeet = blocksTravelled * blockLength;
+    return distanceInFeet;
+  }
+  
+
+function calculatesFarePrice(startblock, endblock) {
+
+    const distanceInFeet= distanceTravelledInFeet(startblock, endblock);
+    
+    if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <=2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
